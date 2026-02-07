56cab5f1d89baf7f2c15fe29fcaf5ae2882b9b92
Jarek <s24468@pjwstk.edu.pl>
2026-02-08 00:04:33 +0100

    Door prefab done

--- Assets/Scripts/EventsForListening/FoodEvents.cs
+++ Assets/Scripts/EventsForListening/FoodEvents.cs
@@ -2,7 +2,6 @@ using System;
 
 public static class FoodEvents
 {
-    // kto zjadł (opcjonalnie), ile, nazwa/ID
     public static event Action<int, string> OnFoodEaten;
 
     public static void RaiseFoodEaten(int hungerGain, string foodName)
---
+++ Assets/Scripts/Interactions/DoorInteraction.cs
@@ -0,0 +1,39 @@
+using DefaultNamespace;
+using DG.Tweening;
+using UnityEngine;
+
+public class DoorInteraction : InteractableBase
+{
+    [Header("Door Settings")] [SerializeField]
+    private float openAngle = 90f;
+    [SerializeField] private float openCloseSpeed = 6f;
+    [SerializeField] private float openCloseDuration = 0.6f;
+    [SerializeField] private Ease ease = Ease.OutQuad;
+    
+    
+    private bool isOpen = false;
+    private Quaternion closedRot;
+    private Quaternion openRot;
+
+    private Tween currentTween;
+    void Awake()
+    {
+        closedRot = transform.rotation;
+        openRot = closedRot * Quaternion.Euler(0f, openAngle, 0f);
+    }
+    
+    protected override void PerformInteract()
+    {
+        isOpen = !isOpen;
+
+        currentTween?.Kill();
+
+        Quaternion target = isOpen ? openRot : closedRot;
+
+        currentTween = transform
+            .DORotateQuaternion(target, openCloseDuration)
+            .SetEase(ease);
+
+        Debug.Log(isOpen ? "Otwarto drzwi" : "Zamknięto drzwi");
+    }
+}
